e19060b72eee0c70a7870c4b5faa535b24acbe2c
Nahedh-BenAbbes <nahedhabs@gmail.com>
2020-11-20 15:47:23 +0100

    employee

---
+++ src/Components/My Employee/Employee.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Employee extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          username: '',
+        };
+      }
+     
+      
+      render() {
+        return (
+          <div>
+           <h1>Hello</h1>
+          </div>
+         
+        );
+      }
+    }
+    
+  export default Employee;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Messageform from '../Message/Messageform.jsx';
 import Dashboard from "../Dashboard/Dashboard.jsx";
 import Board from '../Profile/Board.jsx';
+import Employee from '../My Employee/Employee.jsx';
     class Navbar extends Component {
         constructor(props) {
           super(props);
@@ -14,6 +15,7 @@ import Board from '../Profile/Board.jsx';
           this.profile = this.profile.bind(this);
           this.Dashboard = this.Dashboard.bind(this);
           this.message = this.message.bind(this);
+          this.Employee = this.Employee.bind(this);
         }
         componentDidMount() {
           this.setState({
@@ -38,6 +40,12 @@ import Board from '../Profile/Board.jsx';
             check: "message",
           });
         }
+        Employee(e) {
+          e.preventDefault();
+          this.setState({
+            check: "Employee",
+          });
+        }
       
       
         render() {
@@ -60,7 +68,10 @@ import Board from '../Profile/Board.jsx';
                     {" "}
                     My messages
                   </span>
-                 
+                  <span className="nav"  className="nav-selected" onClick={(e) => this.message(e)}>
+                    {" "}
+                    My Employee
+                  </span>
 
                 </div>
                 <Dashboard email={this.state.email} password={this.state.password}/>
@@ -88,7 +99,10 @@ import Board from '../Profile/Board.jsx';
                     My messages
                   </span>
                  
-          
+                  <span className="nav"  className="nav-selected" onClick={(e) => this.message(e)}>
+                    {" "}
+                    My Employee
+                  </span>
                 </div>
                 <Board email={this.state.email} password={this.state.password}/>
 
@@ -114,12 +128,42 @@ import Board from '../Profile/Board.jsx';
                     My messages
                   </span>
                  
-
+                  <span className="nav"  className="nav-selected" onClick={(e) => this.message(e)}>
+                    {" "}
+                    My Employee
+                  </span>
                 </div>
                 <Messageform email={this.state.email} password={this.state.password}/>
               </div>
             );
-          }
+          } else if (this.state.check === "Employee") {
+            return (
+              <div>
+                 <div className="navbar">
+                  <span className="logo" onClick={(e) => this.profile(e)}>
+                  < img src="https://cdn1.iconfinder.com/data/icons/digital-marketing-44/64/72-512.png" width="40px"></img>
+                  </span>
+                  <span
+                    className="nav"
+                    className="nav-selected"
+                    onClick={(e) => this.Dashboard(e)}
+                  >
+                    My Dashboard
+                  </span>
+                  <span className="nav"  className="nav-selected" onClick={(e) => this.message(e)}>
+                    {" "}
+                    My messages
+                  </span>
+                  <span className="nav"  className="nav-selected" onClick={(e) => this.message(e)}>
+                    {" "}
+                    My Employee
+                  </span>
+
+                </div>
+                <Employee/>
+              </div>
+            );
+          } 
           }
         }
       
