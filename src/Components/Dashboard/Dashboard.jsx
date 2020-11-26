6f117609504ef33b9600735cdb31f34a4a241b8c
Ahmed Kortas <42817639+ahmedkortas@users.noreply.github.com>
2020-11-26 15:57:42 +0100

    fixed the navbar and server issue

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -2,49 +2,17 @@ import React, { Component } from "react";
 
 import "../Dashboard/style.css";
 
-
-import {  Card } from 'react-bootstrap';
+import { Card } from "react-bootstrap";
 import { MDBBtn } from "mdbreact";
 import axios from "axios";
 import Swal from "sweetalert2";
 class Dashboard extends Component {
-constructor(props) {
-super(props);
-this.state = {
-view: "overview",
-username: "",
-usertitle: "",
-};
-this.setView = this.setView.bind(this);
-this.currentView = this.currentView.bind(this);
-this.profile = this.profile.bind(this);
-}
-
-setView(view) {
-this.setState({ view: view });
-}
-currentView(view) {
-this.setState({ view: view });
-}
-profile(e) {
-e.preventDefault();
-this.setState({
-check: "profile",
-});
-}
-
-class Dashboard extends Component {
-    
-    
-    
   constructor(props) {
     super(props);
     this.state = {
       view: "overview",
       username: "",
       usertitle: "",
-      profileImage: "",
-     
     };
     this.setView = this.setView.bind(this);
     this.currentView = this.currentView.bind(this);
@@ -63,6 +31,7 @@ class Dashboard extends Component {
       check: "profile",
     });
   }
+
   handleImage = (profileImage) => {
     this.setState({
       profileImage,
@@ -122,14 +91,15 @@ class Sidebar extends React.Component {
           setView={this.props.setView}
         />
         <div>
-        <a href = '/'>
-          <MDBBtn color="warning"
-             onClick={() => {
+          <a href="/">
+            <MDBBtn
+              color="warning"
+              onClick={() => {
                 window.location.reload();
               }}
-          >
-        LOGOUT
-          </MDBBtn>
+            >
+              LOGOUT
+            </MDBBtn>
           </a>
         </div>
       </div>
@@ -143,107 +113,8 @@ class UserProfileView extends React.Component {
   }
 
   render() {
-  
     return (
       <div className="user-profile">
-
-        {/* 
-                        {/* <img 
-
-            }
-        
-            setView(view) {
-                this.setState({ view: view });
-              
-            }
-                currentView(view) {
-                    this.setState({ view: view });
-                }
-                    profile(e) {
-                        e.preventDefault();
-                        this.setState({
-                          check: "profile",
-                        });
-                      }
-                   
-            
-        
-            render() {
-                switch(this.state.view) {
-                    case "overview":
-                        return (
-                        <div id="dashboard">
-                           
-                            <Sidebar setView={this.setView} />
-                            <Overview />
-                        </div>
-                    );
-                    case "schedule":
-                    return (
-                        <div id="dashboard">
-                            <Sidebar setView={this.setView} />
-                            <ScheduleView />
-                        </div>
-                    );
-                    case "performance":
-                    return (
-                        <div id="dashboard">
-                            <Sidebar setView={this.setView} />
-                            <PerformanceView />
-                        </div>
-                    );
-                case "administrator":
-                    return (
-                        <div id="dashboard">
-                            <Sidebar setView={this.setView} />
-                            <AdministratorView />
-                        </div>
-                    );
-                }
-            }
-        }
-        
-        class Sidebar extends React.Component {
-            constructor(props) {
-                super(props);
-            }
-            setView(view) {
-                this.props.setView(view);
-                console.log("level 2 " + view);
-            }
-            render() {
-                return (
-                    <div className="sidebar-menu">
-                        <UserProfileView  />
-                        <SidebarMenu
-                            item1={"Add Client"}
-                            item2={"Add Employee"}
-                            item3={"Update Profile"}
-                            item4 ={"Add Admin"}
-                            setView={this.props.setView}
-                        />
-                        <div>
-                        <button onClick={()=>{window.location.reload()}}><a>LOGOUT</a></button>
-                        </div>
-                    </div>
-                );
-            }
-        }
-        
-        class UserProfileView extends React.Component {
-            constructor(props) {
-                super(props);
-            }
-        
-            render() {
-                return (
-                    <div className="user-profile">
-                        <img 
-                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQ2wn18dnC8OmX7Qx49epjgoHREUBHEviB10griBGemOmkYQoK5g"
-                            id="profile-pic"
-                           
-                        /> */}
-        {/* <ProfilePicChanger handleImageChange = {this.handleImage} pic1={pic1} pic2={pic2}/> */}
         <h3 id="display-name">{this.props.username}</h3>
         <p className="subtitle">{this.props.usertitle}</p>
       </div>
@@ -297,79 +168,70 @@ class SidebarMenu extends React.Component {
   }
 
   render() {
-      
     return (
       <div className="menu-items">
         <a
           className={this.state.overview}
           href="#"
-          onClick={() => this.setBtnAndView("overview")}>
+          onClick={() => this.setBtnAndView("overview")}
+        >
           {this.props.item1}
         </a>
         <a
           className={this.state.schedule}
           href="#"
-          onClick={() => this.setBtnAndView("schedule")}>
+          onClick={() => this.setBtnAndView("schedule")}
+        >
           {this.props.item2}
         </a>
         <a
           className={this.state.performance}
           href="#"
-          onClick={() => this.setBtnAndView("performance")}>
+          onClick={() => this.setBtnAndView("performance")}
+        >
           {this.props.item3}
         </a>
-       
       </div>
     );
   }
 }
 
 class Overview extends React.Component {
-   
-      
   constructor(props) {
     super(props);
-    
-
   }
   render() {
-    
     return (
       <div className="dash-view">
-          
         <center>
-          <Card className="bg-dark text-white" style = {{height : '600px'}}>
+          <Card className="bg-dark text-white" style={{ height: "600px" }}>
             <Card.ImgOverlay>
               <Card.Title>
-              <h2 className="view-heading" style = {{color :'gold'}}>Add Client</h2>
+                <h2 className="view-heading" style={{ color: "gold" }}>
+                  Add Client
+                </h2>
               </Card.Title>
               <form>
-            
-            <br></br>
-            <input type="text" placeholder="FirstName" required />
-            <br></br>
-            <br></br>
-            <input type="text" placeholder="LastName" required />
-            <br></br>
-            <br></br>
-            <input type="text" placeholder="email" required />
-            <br></br>
-            <br></br>
-            <input type="password" placeholder="Password" required />
-            <br></br>
-            <br></br>
-            <input type="text" placeholder="PhoneNumber" required />
-            <br></br>
-            <br></br>
-            <MDBBtn color="warning">Add</MDBBtn>
-
-           
-           
-          </form>
+                <br></br>
+                <input type="text" placeholder="FirstName" required />
+                <br></br>
+                <br></br>
+                <input type="text" placeholder="LastName" required />
+                <br></br>
+                <br></br>
+                <input type="text" placeholder="email" required />
+                <br></br>
+                <br></br>
+                <input type="password" placeholder="Password" required />
+                <br></br>
+                <br></br>
+                <input type="text" placeholder="PhoneNumber" required />
+                <br></br>
+                <br></br>
+                <MDBBtn color="warning">Add</MDBBtn>
+              </form>
             </Card.ImgOverlay>
           </Card>
-
-          
         </center>
         <DashboardCard />
       </div>
@@ -385,64 +247,40 @@ class ScheduleView extends React.Component {
     return (
       <div className="dash-view">
         <center>
-        <Card className="bg-dark text-white" style = {{height : '600px'}}>
+          <Card className="bg-dark text-white" style={{ height: "600px" }}>
             <Card.ImgOverlay>
               <Card.Title>
-              <h2 className="view-heading" style = {{color :'gold'}}>Add Employee</h2>
+                <h2 className="view-heading" style={{ color: "gold" }}>
+                  Add Employee
+                </h2>
               </Card.Title>
               <form>
-              
-          <br></br>
-          <input type="text" placeholder="FirstName" required />
-          <br></br>
-          <br></br>
-          <input type="text" placeholder="LastName" required />
-          <br></br>
-          <br></br>
-          <input type="text" placeholder="email" required />
-          <br></br>
-          <br></br>
-          <input type="password" placeholder="Password" required />
-          <br></br>
-          <br></br>
-          <input type="text" placeholder="PhoneNumber" required />
-          <br></br>
-          <br></br>
+                <br></br>
+                <input type="text" placeholder="FirstName" required />
+                <br></br>
+                <br></br>
+                <input type="text" placeholder="LastName" required />
+                <br></br>
+                <br></br>
+                <input type="text" placeholder="email" required />
+                <br></br>
+                <br></br>
+                <input type="password" placeholder="Password" required />
+                <br></br>
+                <br></br>
+                <input type="text" placeholder="PhoneNumber" required />
+                <br></br>
+                <br></br>
 
-          <MDBBtn color="warning">Add</MDBBtn>
- 
-            <br></br>
-            <br></br>
-            <br></br>
+                <MDBBtn color="warning">Add</MDBBtn>
 
-           
-           
-          </form>
+                <br></br>
+                <br></br>
+                <br></br>
+              </form>
             </Card.ImgOverlay>
           </Card>
-        class PerformanceView extends React.Component {
-            constructor(props) {
-                super(props);
-            }
-            render() {
-                return (
-                    <div className="dash-view">
-                        <h2 className="view-heading">Update Profile</h2>
-                        <input type="text" placeholder="email" required/><br></br><br></br>
-                        <input type="password" placeholder="Password" required/><br></br><br></br>
-                        <input type="password" placeholder="NewPassword" required/><br></br><br></br>
-                        <input type="password" placeholder="NewPassword" required/><br></br><br></br>
-                        <input type="button" value="Save"/>                          
-                        <DashboardCard />
-                    </div>
-                );
-            }
-        }
-        
- 
-
-          
-                 </center>
+        </center>
         <DashboardCard />
       </div>
     );
@@ -456,68 +294,47 @@ class PerformanceView extends React.Component {
   render() {
     return (
       <div className="dash-view">
-           <center>
-        <Card className="bg-dark text-white" style = {{height : '600px'}}>
+        <center>
+          <Card className="bg-dark text-white" style={{ height: "600px" }}>
             <Card.ImgOverlay>
               <Card.Title>
-              <h2 className="view-heading" style = {{color :'gold'}}>Edit Profile</h2>
+                <h2 className="view-heading" style={{ color: "gold" }}>
+                  Edit Profile
+                </h2>
               </Card.Title>
               <form>
-              
-          <br></br>
-          <input type="text" placeholder="email" required />
-        <br></br>
-        <br></br>
-        <input type="password" placeholder="Password" required />
-        <br></br>
-        <br></br>
-        <input type="password" placeholder="NewPassword" required />
-        <br></br>
-        <br></br>
-        <input type="password" placeholder="NewPassword" required />
-        <br></br>
-        <br></br>
-        <input type="button" value="Save" />
-       <br></br>
-          <br></br>
+                <br></br>
+                <input type="text" placeholder="email" required />
+                <br></br>
+                <br></br>
+                <input type="password" placeholder="Password" required />
+                <br></br>
+                <br></br>
+                <input type="password" placeholder="NewPassword" required />
+                <br></br>
+                <br></br>
+                <input type="password" placeholder="NewPassword" required />
+                <br></br>
+                <br></br>
+                <input type="button" value="Save" />
+                <br></br>
+                <br></br>
 
-          <MDBBtn color="warning">Add</MDBBtn>
- 
-            <br></br>
-            <br></br>
-            <br></br>
+                <MDBBtn color="warning">Add</MDBBtn>
 
-           
-           
-          </form>
+                <br></br>
+                <br></br>
+                <br></br>
+              </form>
             </Card.ImgOverlay>
           </Card>
+        </center>
 
-          
-                 </center>
-        
         <DashboardCard />
       </div>
     );
   }
 }
-        class AdministratorView extends React.Component {
-            constructor(props) {
-                super(props);
-            }
-            render() {
-                return (
-                    <div className="dash-view">
-                       <h2 className="view-heading">Add Admin</h2>
-                        <input type="text" placeholder="email" required/><br></br><br></br>
-                        <input type="password" placeholder="Password" required/><br></br><br></br>
-                        <DashboardCard />
-                    </div>
-                );
-            }
-        }
-        var currentView = "overview";
-
 class AdministratorView extends React.Component {
   constructor(props) {
     super(props);
@@ -525,7 +342,14 @@ class AdministratorView extends React.Component {
   render() {
     return (
       <div className="dash-view">
-       
+        <h2 className="view-heading">Add Admin</h2>
+        <input type="text" placeholder="email" required />
+        <br></br>
+        <br></br>
+        <input type="password" placeholder="Password" required />
+        <br></br>
+        <br></br>
+        <DashboardCard />
       </div>
     );
   }
@@ -573,4 +397,3 @@ const PerformanceCardContent = () => <div></div>;
 const AdministratorCardContent = () => <div></div>;
 
 export default Dashboard;
-
--- src/index.jsx
+++ src/index.jsx
@@ -1,70 +1,59 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Login from "./Components/Login/Login.jsx";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Land from "./Components/Landing/Landingpage.jsx";
-import './Components/My Employee/App.css';
-import './Components/Profile/style.css';
-import '@fortawesome/fontawesome-free/css/all.min.css'; 
-import Messageform from './Components/Message/Messageform';
+import "./Components/My Employee/App.css";
+import "./Components/Profile/style.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import Messageform from "./Components/Message/Messageform";
 import Dashboard from "./Components/Dashboard/Dashboard";
-import Profile from './Components/Profile/Profile';
-import Employee from './Components/My Employee/Employee';
-import Navbar from './Components/Navbar/Navbar'
-import Client from './Components/Client/Client';
-import Chat from './Components/Calendar/Chat';
-import Task from './Components/Tasks/task';
+import Profile from "./Components/Profile/Profile";
+import Employee from "./Components/My Employee/Employee";
+import Navbar from "./Components/Navbar/Navbar";
+import Client from "./Components/Client/Client";
+import Chat from "./Components/Calendar/Chat";
+import Task from "./Components/Tasks/task";
 import "bootswatch/dist/lux/bootstrap.min.css";
-// import 'bootstrap-css-only/css/bootstrap.min.css'; 
-import 'mdbreact/dist/css/mdb.css';
-import 'antd/dist/antd.css';
-import {Avatar} from 'antd';
-import { UserOutlined } from '@ant-design/icons';
- import ProfilePicChanger from './Components/ProfilePicChanger'
-import pic1 from '../src/pics/tiger.PNG'
-import pic2 from '../src/pics/suited.jpg'
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
+import "antd/dist/antd.css";
 
 class App extends React.Component {
-    constructor(props){
-            super(props);
-            this.state = {
-                profileImage : ''
-            }
-    }
-    handleImage =(profileImage) =>{
-        this.setState ({
-            profileImage
-        })
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      profileImage: "",
+    };
+  }
+  handleImage = (profileImage) => {
+    this.setState({
+      profileImage,
+    });
+  };
 
-    render(){
-        return (
-            <div>
-                <Navbar/>
-                {/* <Avatar size={64} icon={<UserOutlined />} src={this.state.profileImage} />
-                <ProfilePicChanger handleImageChange = {this.handleImage} pic1={pic1} pic2={pic2}/> */}
-               
+  render() {
+    return (
+      <div>
         <Router>
-        <div>
-            
+          <div>
+            <Navbar />
             <Switch>
-
-                <Route path='/Login' exact component={Login}/>
-                <Route path='/' exact component={Land}/>
-                <Route path ='/dashboard' component = {Dashboard}></Route>
-                <Route path ='/messages' component = {Messageform}></Route>
-                <Route path ='/clients' component = {Client}></Route>
-                <Route path ='/employees' component = {Employee}></Route>
-                <Route path ='/profile' component = {Profile}></Route>
-                <Route path ='/chat' component = {Chat}></Route>
-                <Route path ='/tasks' component = {Task}></Route>
+              <Route path="/Login" exact component={Login} />
+              <Route path="/" exact component={Land} />
+              <Route path="/dashboard" component={Dashboard}></Route>
+              <Route path="/messages" component={Messageform}></Route>
+              <Route path="/clients" component={Client}></Route>
+              <Route path="/employees" component={Employee}></Route>
+              <Route path="/profile" component={Profile}></Route>
+              <Route path="/chat" component={Chat}></Route>
+              <Route path="/tasks" component={Task}></Route>
             </Switch>
-        </div>
-    </Router>
-    </div>
-        )
-    }
-
+          </div>
+        </Router>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
